af7c16b91c1981b756261f92eb672029aa3690f4
Gisellrom <romero.giselln@gmail.com>
2024-03-24 23:08:22 -0600

    cambia expresión regular

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function encriptar(){
     let parrafo = document.getElementById("parrafo");
     let imgChico = document.getElementById("chico");
     
-    var regex = /^[a-z\s!¡¿?]*$/i;
+    var regex = /^[a-z\s!¡¿?]*$/;
 
     // Verificar si el texto coincide con la expresión regular
     if (regex.test(texto)) {
